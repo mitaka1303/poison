81060667142148121cd474cd1fd87a06972dc86a
mitaka1303 <mitaka1332@outlook.jp>
2025-02-03 03:09:38 +0900

    color test

---
+++ src/form.jsx
@@ -0,0 +1,26 @@
+export default function Form(props) {
+
+    return(
+
+        <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+
+
+    )
+  }
+
+  
