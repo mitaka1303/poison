1e29ecaf611fdd8ecfe42f74f9a95d2bf8ea0721
mitaka1303 <mitaka1332@outlook.jp>
2025-02-09 21:52:57 +0900

    color test

---
+++ src/toxic_form.jsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+
+export default function toxicForm(props) {
+    const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    props.set_toxic_way(event.target.value);
+  };
+
+    return(
+        <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">toxic1</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>刺毒,咬毒</MenuItem>
+          <MenuItem value={20}>粘液毒</MenuItem>
+          <MenuItem value={30}>食中毒</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+    )
+  }
+
+  
