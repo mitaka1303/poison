06b3ce9feb3d236cdac306eabff59466bea6d2f8
mitaka1303 <mitaka1332@outlook.jp>
2025-02-10 23:45:18 +0900

    color test

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,18 @@ function App() {
   const [toxic_way, set_toxic_way] = useState(10)
 
   const [toxic_color1, set_toxic_color1] = useState([255, 2, 200])
+  const [toxic_color2, set_toxic_color2] = useState([2, 200, 200])
 
-  const color1 = [255, 2, 200]
-  const color2 = [100 , 21, 100]
 
-  const colorData = color_mixer(toxic_color1, color2);
+  const colorData = color_mixer(toxic_color1, toxic_color2);
 
 
   return (
     <>
       <div>
         <Form set_toxic_way={set_toxic_way}/>
+        <toxic_form set_toxic_color1={set_toxic_color1}/>
+        <toxic_form set_toxic_color2={set_toxic_color2}/>
         <IconRender colorData = {colorData} toxic_way={toxic_way} />
       </div>
     </>
